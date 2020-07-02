70ab33499820344aadf9160d0cff61c454969a24
Saajan Kuthu <kuthusaju@gmail.com>
2020-07-02 11:57:25 +1000

    initial commint

---
+++ HMI/Scripts/AssignKeyToButton.js
@@ -0,0 +1,64 @@
+﻿// Keep this lines for a best effort IntelliSense of Visual Studio 2017.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.d.ts" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Controls\System\TcHmiControl\Source.d.ts" />
+
+// Keep this lines for a best effort IntelliSense of Visual Studio 2013/2015.
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\Lib\jquery\jquery.js" />
+/// <reference path="C:\TwinCAT\Functions\TE2000-HMI-Engineering\Infrastructure\TcHmiFramework\Latest\TcHmi.js" />
+
+(function (TcHmi) {
+
+    var AssignKeyToButton = function (Button, Key) {
+
+        var buttonId = Button.getId();
+        var triggeredKeyDown = false;
+        var triggeredKeyPress = false;
+
+        $(document).on("keydown." + buttonId, function (event) {
+            var key = event.key;
+            if (key == Key) {
+                if (!triggeredKeyDown) {
+                    triggeredKeyDown = true;
+                    TcHmi.EventProvider.raise(buttonId + '.onMouseDown');
+                    $("#" + buttonId).addClass("down");
+                }
+            }        
+        });
+
+        $(document).on("keyup." + buttonId, function (event) {
+            var key = event.key;
+            if (key == Key) {
+                triggeredKeyDown = false;
+                triggeredKeyPress = false;
+                TcHmi.EventProvider.raise(buttonId + '.onMouseUp');
+                $("#" + buttonId).removeClass("down");
+            }
+        });
+
+        $(document).on("keypress." + buttonId, function (event) {
+            var key = event.key;
+            if (key == Key) {
+                if (!triggeredKeyPress) {
+                    triggeredKeyPress = true;
+                    TcHmi.EventProvider.raise(buttonId + '.onPressed');
+                }
+            }
+        });
+
+        var destroyDetachedEvent = TcHmi.EventProvider.register(buttonId + '.onDetached', function (evt, data) {
+
+                // unregister the events when the control is detached.
+                $(document).off("keydown." + buttonId);
+                $(document).off("keyup." + buttonId);
+                $(document).off("keypress." + buttonId);
+
+                // destroy the onDeteached call back function
+                destroyDetachedEvent();
+            }
+        );
+
+    };
+    
+    TcHmi.Functions.registerFunction('AssignKeyToButton', AssignKeyToButton);
+})(TcHmi);
---
+++ README.md
@@ -0,0 +1,33 @@
+# Example Keyboard Press Buttons
+
+## Disclaimer
+This is a personal guide not a peer reviewed journal or a sponsored publication. We make
+no representations as to accuracy, completeness, correctness, suitability, or validity of any
+information and will not be liable for any errors, omissions, or delays in this information or any
+losses injuries, or damages arising from its display or use. All information is provided on an as
+is basis. It is the reader’s responsibility to verify their own facts.
+
+The views and opinions expressed in this guide are those of the authors and do not
+necessarily reflect the official policy or position of any other agency, organization, employer or
+company. Assumptions made in the analysis are not reflective of the position of any entity
+other than the author(s) and, since we are critically thinking human beings, these views are
+always subject to change, revision, and rethinking at any time. Please do not hold us to them
+in perpetuity.
+
+## Overview 
+Demo of showing a method of using JavaScript add binding to a key stroke.
+
+AssignKeyToButton.js has been created to take a Tchmi Button control and a key.  This can be found in each of the buttons .onAttached.
+
+This code supports .onPressed .onMouseDown .onMouseUp
+
+## Install 
+Not required.  Simply open the project.
+
+## TwinCAT
+This project uses TcXaeShell 3.1.4024.10 and TwinCAT HMI 1.10.1336.404
+
+## Getting started
+This is not a guide for TcXaeShell, please visit http://beckhoff.com/ for further guides
+* Open the HMI in live view
+* Press a b or c to see results 
